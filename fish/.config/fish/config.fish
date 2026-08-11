4b033c37d07c52e5746c28854f609b3a065a569f
Xenofon Vourliotis <x.vourliotis@gmail.com>
2026-08-11 22:59:47 +0100

    feat(fish): Add terminal title function

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,18 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 function fish_greeting
 end
 
+# Window/tab title: show the running command, or just the current folder's name
+function fish_title
+    set -l cmd (status current-command)
+    if test -n "$cmd"; and test "$cmd" != fish
+        echo $cmd
+    else if test "$PWD" = "$HOME"
+        echo "~"
+    else
+        basename "$PWD"
+    end
+end
+
 set -gx EDITOR nvim
 set -gx SUDO_EDITOR "nvim -u $HOME/.config/nvim/init.lua"
 set -gx FZF_DEFAULT_OPTS '--color=pointer:#B5BD68'
